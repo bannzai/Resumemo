688b4145690763390bfa96e782e70cd86eddada0
bannzai <kingkong999yhirose@gmail.com>
2022-08-11 07:43:13 +0900

    Fix manifest

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { crx, defineManifest } from "@crxjs/vite-plugin";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: "Resumemo",
+  version: "1.0.0",
+  // Ref: https://developer.chrome.com/docs/extensions/mv3/declare_permissions/
+  permissions: ["activeTab", "storage", "identity"],
+  action: {
+    default_popup: "index.html",
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), crx({ manifest })],
+});
