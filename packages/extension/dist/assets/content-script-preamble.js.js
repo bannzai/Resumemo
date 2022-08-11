25fe504db1ff550ef350aad1d0bc9bb31efcbda0
bannzai <kingkong999yhirose@gmail.com>
2022-08-11 11:50:13 +0900

    :shell: git rm -r --cached dist

--- packages/extension/dist/assets/content-script-preamble.js.js
+++
@@ -1,6 +0,0 @@
-import exports from '../vendor/react-refresh.js';
-
-exports.injectIntoGlobalHook(window);
-window.$RefreshReg$ = () => {};
-window.$RefreshSig$ = () => (type) => type;
-window.__vite_plugin_react_preamble_installed__ = true;
